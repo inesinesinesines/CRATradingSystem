dc438745a2165cd1eace4a9a086355e1b4a73022
inhae.kang <kang.inhae@gmail.com>
2025-07-28 09:17:10 +0900

    commit Project init code

---
+++ main.cpp
@@ -0,0 +1,7 @@
+#include "gmock/gmock.h"
+
+int main(void)
+{
+	::testing::InitGoogleMock();
+	return RUN_ALL_TESTS();
+}
---
+++ test.cpp
@@ -0,0 +1,9 @@
+#include "gmock/gmock.h"
+#include <string>
+
+TEST(TC, TC1) {
+	AutoTradingSystem* ats = new AutoTradingSystem();
+	std::string broker = "mock"
+	ats->selectStockBroker(broker);
+	EXPECT_EQ(broker, ats->checkCurrentBroker());
+}
