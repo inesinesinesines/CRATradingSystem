f93e82bae2c394c028d61f76f76f70e576dd164a
inhae.kang <kang.inhae@gmail.com>
2025-07-28 10:15:07 +0900

    add brocker driver TC

--- test.cpp
+++ test.cpp
@@ -1,9 +1,58 @@
 #include "gmock/gmock.h"
 #include <string>
 
-TEST(TC, TC1) {
-	AutoTradingSystem* ats = new AutoTradingSystem();
-	std::string broker = "mock"
-	ats->selectStockBroker(broker);
-	EXPECT_EQ(broker, ats->getCurrentBrokerName());
+#include "MockDriver.cpp"
+
+TEST(MockDriverTest, LoginTest) {
+	StockBrockerDriver* sbDriver = new MockDriver();
+	std::string ID = "USER";
+	std::string PASSWORD = "PASSWORD";	
+	std::string STOCKCODE = "SAM";
+	EXPECT_NO_THROW({ sbDriver->login(ID, PASSWORD); });
 }
+
+TEST(MockDriverTest, BuyTest) {
+	StockBrockerDriver* sbDriver = new MockDriver();
+	std::string ID = "USER";
+	std::string PASSWORD = "PASSWORD";
+	std::string STOCKCODE = "SAM";
+	int count = 1000;
+	int price = 50;
+
+	// User should login before request buy 
+	EXPECT_EQ(false, sbDriver->buy(PASSWORD, count, price));
+
+	sbDriver->login(ID, PASSWORD);
+	EXPECT_EQ(true, sbDriver->buy(PASSWORD, count, price));
+}
+
+TEST(MockDriverTest, SellTest) {
+	StockBrockerDriver* sbDriver = new MockDriver();
+	std::string ID = "USER";
+	std::string PASSWORD = "PASSWORD";
+	std::string STOCKCODE = "SAM";
+	int count = 1000;
+	int price = 50;
+
+	// User should login before request buy 
+	EXPECT_EQ(false, sbDriver->sell(PASSWORD, count, price));
+
+	sbDriver->login(ID, PASSWORD);
+	EXPECT_EQ(true, sbDriver->sell(PASSWORD, count, price));
+}
+
+TEST(MockDriverTest, GetPrice) {
+	StockBrockerDriver* sbDriver = new MockDriver();
+	std::string ID = "USER";
+	std::string PASSWORD = "PASSWORD";
+	std::string STOCKCODE = "SAM";
+	int count = 1000;
+	int price = 50;
+
+	// User should login before request buy 
+	EXPECT_EQ(false, sbDriver->getPrice(PASSWORD, count, price));
+
+	sbDriver->login(ID, PASSWORD);
+	EXPECT_EQ(true, sbDriver->getPrice(PASSWORD, count, price));
+}
+
